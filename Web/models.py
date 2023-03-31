8d728effcb4bf7438291e1a4129b94bf3caa46b1
Alireza.Sepehri <alireza.sepehri1388@gmail.com>
2023-03-31 23:29:45 +0430

    append Incom

--- Web/models.py
+++ Web/models.py
@@ -11,3 +11,12 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
+class Incom(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
